38ded09c70d27b2f45bff0277faacaa95fae5efe
Paarth <paarth.n@gmail.com>
2016-08-10 08:34:21 +0530

    BW image patch

--- utils.py
+++ utils.py
@@ -3,6 +3,7 @@ from scipy import misc
 
 # VGG 16 accepts RGB channel 0 to 1 (This tensorflow model).
 def load_image_array(image_file):
-	img = misc.imread(image_file)
+	print "Image File", image_file
+	img = misc.imread(image_file, mode='RGB')
 	img_resized = misc.imresize(img, (224, 224))
 	return (img_resized/255.0).astype('float32')
